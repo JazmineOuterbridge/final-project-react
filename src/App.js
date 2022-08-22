ced1018f1294c4b6cfac7623703c551d16909ef5
JazmineOuterbridge <108620390+JazmineOuterbridge@users.noreply.github.com>
2022-08-22 12:26:17 -0300

    added space

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
 <footer> This project was coded by
   <a className='text-decoration-none' href='https://deft-bienenstitch-44c693.netlify.app/' target="blank">{" "}Jazmine Outerbridge</a> and is
       <a className='text-decoration-none' href='https://github.com/JazmineOuterbridge/final-project-react' target="blank">{" "} Open-sourced</a>
-       and hosted on {" "}
+      {" "} and hosted on {" "}
        <a className='text-decoration-none' href='https://www.netlify.com/' target="blank">Netlify</a></footer>
     </div></div>
   );
