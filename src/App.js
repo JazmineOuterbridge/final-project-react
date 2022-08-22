9864568f511ed40f34009712ed811313294f87f8
JazmineOuterbridge <108620390+JazmineOuterbridge@users.noreply.github.com>
2022-08-22 12:25:43 -0300

    added link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
      <Weather defaultCity="Bermuda"/>
 <footer> This project was coded by
   <a className='text-decoration-none' href='https://deft-bienenstitch-44c693.netlify.app/' target="blank">{" "}Jazmine Outerbridge</a> and is
-      <a className='text-decoration-none' href='https://github.com/JazmineOuterbridge/final-project-react' target="blank">{" "}Open-sourced</a>
+      <a className='text-decoration-none' href='https://github.com/JazmineOuterbridge/final-project-react' target="blank">{" "} Open-sourced</a>
        and hosted on {" "}
        <a className='text-decoration-none' href='https://www.netlify.com/' target="blank">Netlify</a></footer>
     </div></div>
